8297d613246a26ba7698dfd2f4bdf18e43130fc9
Wei Ning <macgnw@gmail.com>
2019-01-19 17:02:38 +1100

    add source

--- scripts/functions/concept.rs
+++ scripts/functions/concept.rs
@@ -1,5 +1,8 @@
 //$(which true); dst=/var/tmp/sut; out=${dst}/$0.bin; mkdir -p ${dst}; rustc -o "${out}" 1>&2 "$0" && "${out}" "$@"; exit $?
 
+// source
+// https://doc.rust-lang.org/book/ch03-03-how-functions-work.html
+
 fn main() {
     another_function(5);
 }
