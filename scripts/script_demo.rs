f38ba08a9453c398d0e37c3056d2db12c1a75501
Wei Ning <macgnw@gmail.com>
2019-01-21 09:01:02 +1100

    slightly improve script header

--- scripts/script_demo.rs
+++ scripts/script_demo.rs
@@ -1,4 +1,6 @@
-//$(which true); dst=/var/tmp/sut; out=${dst}/$0.bin; mkdir -p ${dst}; rustc -o "${out}" 1>&2 "$0" && "${out}" "$@"; exit $?
+//$(which true); dst=/var/tmp/sut; out=${dst}/$0.bin; 
+//$(which mkdir) -p ${dst}; 
+//$(which rustc) -o "${out}" 1>&2 "$0" && "${out}" "$@"; exit $?
 
 fn main() {
     println!("Hello, rust scripting.");
