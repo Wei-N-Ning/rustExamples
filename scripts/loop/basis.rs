49b5b35963642a04ceeac22296adb36ac419c4a9
Wei Ning <wei@canva.com>
2019-01-21 10:27:20 +1100

    working on loop

--- scripts/loop/basis.rs
+++ scripts/loop/basis.rs
@@ -2,6 +2,62 @@
 //$(which mkdir) -p ${dst}; 
 //$(which rustc) -o "${out}" 1>&2 "$0" && "${out}" "$@"; exit $?
 
+fn loop_loop(num: i8) {
+    let mut counter = num;
+    loop {
+        if counter <= 0 {
+            return;
+        }
+        print!(".");
+        counter -= 1;
+    }
+}
+
+fn for_loop(num: i8) {
+    // .. creates a range object (for in is a syntactic sugar)
+    // for supports iterator 
+    for _n in 0..num {
+        print!("-");
+    }
+}
+
+fn while_loop(num: i8) {
+    let mut counter = num;
+    while counter > 0 {
+        print!("/");
+        counter -= 1;
+    }
+}
+
+fn vector_loop(nums: Vec<i32>) {
+    for elem in nums {
+        println!("{}", elem);
+    }
+}
+
+fn continue_break(nums: Vec<i32>) {
+    for elem in nums {
+        if elem / 2 == 0 {
+            continue;
+        }
+        if elem 
+        println("{}", elem);
+    }
+}
+
 fn main() {
-    
+    loop_loop(3);
+    for_loop(3);
+    while_loop(3);
+
+    // prefer the micro 
+    // let mut nums = Vec::new();
+    // nums.push(1);
+    // nums.push(2);
+    let nums = vec![1, 2];
+    vector_loop(nums);
+
+    continue_break();
+
+    println!("+ done");
 }
