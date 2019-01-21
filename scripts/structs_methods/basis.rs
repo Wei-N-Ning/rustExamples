428f15811a678aac5b0cf343f2b04f8318dbaac9
Wei Ning <wei@canva.com>
2019-01-22 10:15:08 +1100

    rs in 7d

--- scripts/structs_methods/basis.rs
+++ scripts/structs_methods/basis.rs
@@ -4,16 +4,44 @@
 
 // to use this struct outside the project:
 // pub struct {}
-#[derive(Debug)]
+
+// add this: #[derive(Debug)]
+// then the object can be converted to string (for debugging)
+// {:?}
+
 struct Item {
     name: String,
     size: u32,
 }
 
+impl Item {
+    // self: object
+    // &self: const pointer
+    // &mut self: pointer
+    fn to_string(&self) -> String {
+        format!("<{}, {}>", self.name, self.size)
+    }
+
+    fn set_size(&mut self, size: u32) {
+        self.size = size;
+    }
+
+    // consumer method use move semantic to "consume" the
+    // object passed in
+    fn destroy(self) {
+    } 
+}
+
 fn main() {
-    let item = Item {
+    let mut item = Item {
         name: String::from("øπ"),
         size: 213,
     };
-    println!("{:?}", item);
+    item.set_size(123);
+    println!("{}", item.to_string());
+
+    item.destroy();
+    // item.set_size(456);  // compiler: value used here after move
 }
+
+
