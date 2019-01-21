19dc7bb321548d143fafec3b750b9663df470ff4
Wei Ning <wei@canva.com>
2019-01-22 10:17:29 +1100

    rs in 7d

---
+++ scripts/enums/basis.rs
@@ -0,0 +1,13 @@
+//$(which true); dst=/var/tmp/sut; out=${dst}/$0.bin; 
+//$(which mkdir) -p ${dst}; 
+//$(which rustc) -o "${out}" 1>&2 "$0" && "${out}" "$@"; exit $?
+
+pub enum Room {
+    Kitchen,
+    Bedroom,
+    Lounge,
+}
+
+fn main() {
+    
+}
