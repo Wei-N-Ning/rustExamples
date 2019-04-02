b36215c62039a312d55863c6968b73a6b024cb28
Wei Ning <nwcgnw@gmail.com>
2019-04-02 14:40:57 +1100

    macros

---
+++ scripts/macros/compliation.rs
@@ -0,0 +1,24 @@
+//$(which true); dst=/var/tmp/sut; out=${dst}/$0.bin; 
+//$(which mkdir) -p ${dst}; 
+//$(which rustc) -o "${out}" 1>&2 "$0" && "${out}" "$@"; exit $?
+
+fn platform_agnostic_code() {
+    if cfg!(target_os = "macos") {
+        println!("Mac OS foo");
+    }
+    else if cfg!(target_os = "linux") {
+        let var = LinuxClass {};
+        println!("Linux foo: {:?}", var);
+    }
+    else {
+        println!("Other foo");
+    }
+}
+
+#[cfg(target_os = "linux")]
+#[derive(Debug)]
+struct LinuxClass {}
+
+fn main() {
+    platform_agnostic_code();
+}
