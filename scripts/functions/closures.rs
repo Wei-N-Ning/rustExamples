fef65566f0af981f3d46bf45ab93e9aaefb1d328
Wei Ning <macgnw@gmail.com>
2019-06-01 13:11:36 +1000

    wip

--- scripts/functions/closures.rs
+++ scripts/functions/closures.rs
@@ -2,15 +2,46 @@
 //$(which mkdir) -p ${dst}; 
 //$(which rustc) -o "${out}" 1>&2 "$0" && "${out}" "$@"; exit $?
 
-use std::ops::Fn;
+// - closure is a function that captures its environment
+// - this means it is defined inline with other code and can 
+//   access bindings declared in that code
+// - they are anonymous and their type can not be named (can not 
+//   do "this function takes this type of closure")
+// - a closure implements one of the Fn family of traits,
+//   meaning it can be called with () syntax, like a function
+// - to return it or accept it in a function, must use generic
+//   or dynamic trait object
 
-// a closure implements one of the Fn family of traits,
-// meaning it can be called with () syntax, like a function
-fn demo_closure_creation() {
-    let clo1 = |x| {
-        x + 1
-    };
-    println!("{}", clo1(123));
+// source
+// how to return closure
+// https://doc.rust-lang.org/rust-by-example/fn/closures/output_parameters.html
+
+// Closures as input parameters are possible, so returning closures 
+// as output parameters should also be possible. However, returning 
+// closure types are problematic because Rust currently only supports 
+// returning concrete (non-generic) types. Anonymous closure types 
+// are, by definition, unknown and so returning a closure is only 
+// possible by making it concrete. This can be done via boxing.
+fn demo_closure_creation() -> Box<Fn() -> i32> {
+    let clo1 = |x| { x + 1 };
+    apply(clo1);
+    println!("called: {}", clo1(123));
+
+    Box::new(move || { 10 })
+}
+
+// source
+// how to pass closure to function
+// https://doc.rust-lang.org/rust-by-example/fn/closures/input_parameters.html
+
+// A function which takes a closure as an argument and calls it.
+fn apply<F>(f: F) where
+    // The closure takes no input and returns nothing.
+    F: FnOnce(i32) -> i32
+{
+    // ^ TODO: Try changing this to `Fn` or `FnMut`.
+
+    println!("applied: {}", f(213));
 }
 
 // we can not write the type of a function - it is called
@@ -28,5 +59,6 @@ fn demo_closure_creation() {
 // it creates full OS threads, not green threads (goroutines...)
 
 fn main() {
-    demo_closure_creation();
+    let f = demo_closure_creation();
+    println!("called as return value: {}", f());
 }
--- scripts/types/dynamic_dispatch.rs
+++ scripts/types/dynamic_dispatch.rs
@@ -10,6 +10,32 @@ use std::fmt::Display;
 // e.g. a vector of "trait objects"
 // requires an extra pointer lookup
 // the objects lose their other behavior (such as String::push)
+
+// adding a couple of long notes after watching this episode 
+// again
+
+// this sounds like C++ template
+
+// "hey compiler, this function can handle all kinds of types,
+// but I do not want to write them out, so just figure them out
+// at compile time and generate each version of this function for
+// each of those types"
+
+// this sounds like C++ vtable-based inheritance
+
+// a generic type call &dyn Display; &: reference; Display is 
+// a trait; dyn means to do dynamic dispatch here, with the 
+// performance penalty; "this is not a generic, but any types 
+// that implements the Display trait"; compiler creates a vtable
+// and a pointer to it
+// the heterogeneous objects are created by the caller (the 
+// creator of the vector of Display trait implementer, the so 
+// called "trait objects"), this uses the "as" technique to 
+// dynamic_cast<> various objects, such as i32, String etc.
+// to the trait objects; as with dynamic_cast, the trait object 
+// no longer behaves like the origin copy but its data is still
+// there
+// function_call -> argument (ptr) -> vtable -> function
 fn show_all(v: Vec<&dyn Display>) {
     for item in v {
         println!("{}", item);
