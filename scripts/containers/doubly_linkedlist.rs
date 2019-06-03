9e29ca2eb1654c0946d966e3c1b9d2cae4eba03b
Wei Ning <macgnw@gmail.com>
2019-06-03 18:05:56 +1000

    wip

--- scripts/containers/doubly_linkedlist.rs
+++ scripts/containers/doubly_linkedlist.rs
@@ -2,6 +2,24 @@
 //$(which mkdir) -p ${dst}; 
 //$(which rustc) -o "${out}" 1>&2 "$0" && "${out}" "$@"; exit $?
 
+use std::rc::Rc;
+use std::cell::RefCell;
+
+type Link = Option<Rc<RefCell<Node>>>;
+
+#[derive(Debug, Clone)]
+struct Node {
+    value : String,
+    next : Link,
+    prev : Link,
+}
+
+fn test_node_creation() {
+    let n = Node { value: "AA".to_string(), next: None, prev: None };
+    println!("{:?}", n);
+    assert!(n.next.is_none() && n.prev.is_none());
+}
+
 fn main() {
-    ;
+    test_node_creation();
 }
